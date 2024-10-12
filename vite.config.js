e7a10d9b0ea1ce34b23d57ae77f46fb0551c7a7f
ashuthikole2004 <ashwinithikole@gmail.com>
2024-10-12 09:11:06 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,13 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     hmr: {
-//       overlay: false,
-//     },
-//   },
-//   assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.png", "**/*.webp"],
-
-//   build: {
-//     rollupOptions: {
-//       input: "./src/main.jsx",
-//     },
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
       overlay: false,
     },
-    historyApiFallback: true,  // Ensure this is added for handling 404s in dev
   },
   assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.png", "**/*.webp"],
-  build: {
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
 });
