18dba79da5de201c3f593ddbe94e001baa904cb6
ashuthikole2004 <ashwinithikole@gmail.com>
2024-10-12 08:44:06 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
-      overlay: false
-    }
+      overlay: false,
+    },
   },
-  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.webp'],
+  assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.png", "**/*.webp"],
 
-  
-})
-
+  build: {
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
+});
